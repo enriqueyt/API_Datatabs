3ec24848d051b4e252ad05dc242e71f7763453c4
Enrique Yepez <enriqueyt@gmail.com>
2016-01-22 16:12:10 -0430

    se comenta el envio de imagen base64

--- utils/utilidades.js
+++ utils/utilidades.js
@@ -225,11 +225,10 @@ exports.agregarImagenFlujo = function(flujos){
 
                 for (var i = 0; i < flujo.length; i++) {
 
-                    if(flujo[i].backgroundUrl.length > 0){
+                    /*if(flujo[i].backgroundUrl.length > 0){
                         var res = request('GET', flujo[i].backgroundUrl);
-                        console.log(new Buffer(res.getBody()).toString('base64'))
                         flujo[i].backgroundImg = ("data:" + res.headers["content-type"] + ";base64," + new Buffer(res.getBody()).toString('base64'));
-                    }
+                    }*/
 
                     if(flujo[i].children.length > 0){
                         flujo = recorrerFlujo(flujo[i]);
